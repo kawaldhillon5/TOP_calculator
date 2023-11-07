e0cb8fca75713878604919c66e0a8564bfce2cbc
kawaldhillon05 <kawaldhillon5@gmail.com>
2023-11-07 14:26:29 -0500

    added clear and delete buttons and filled all buttons with text

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const calc = document.querySelector(".main");
-const screen = document.querySelector(".screen");
-const buttons = document.querySelector(".buttons")
+const display = document.querySelector(".display");
+const buttons = document.querySelector(".lower_buttons")
+let button_values = [['1','4','7','.'],['2','5','8','0'],['3','6','9','='],['+','-','×','÷']]; 
 create_buttons();
 
 function create_buttons(){
     let a  = 4;
-    for(let i = 1;i<=a;i++){
+    for(let i = 0;i<a;i++){
         const coloums = document.createElement("div");
         coloums.classList.add("coloum",i);
         buttons.appendChild(coloums);
-        for(let j = 1;j<=a;j++){
-            const button = document.createElement("div");
+        for(let j = 0;j<a;j++){
+            const button = document.createElement("button");
             button.classList.add("button",i+"*"+j);
+            button.textContent = button_values[i][j];
+            console.log(i+j);
             coloums.appendChild(button);
         }
     }
