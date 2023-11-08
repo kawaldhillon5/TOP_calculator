822895ac9c9a8640bbd5d7415ede263537dca8c7
kawaldhillon05 <kawaldhillon5@gmail.com>
2023-11-07 19:01:25 -0500

    formatted display

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const calc = document.querySelector(".main");
-const display = document.querySelector(".display");
+const content = document.querySelector(".content");
 const buttons = document.querySelector(".lower_buttons")
 let button_values = [['1','4','7','.'],['2','5','8','0'],['3','6','9','='],['+','-','×','÷']]; 
 create_buttons();
-
+content.textContent = "7+9";
 function create_buttons(){
     let a  = 4;
     for(let i = 0;i<a;i++){
@@ -14,7 +14,6 @@ function create_buttons(){
             const button = document.createElement("button");
             button.classList.add("button",i+"*"+j);
             button.textContent = button_values[i][j];
-            console.log(i+j);
             coloums.appendChild(button);
         }
     }
